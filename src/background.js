e63ed07707f8400399fbdbd61e623117e5f34fdf
Takashi Sasaki <t.sasaki@bmandw.com>
2015-11-20 12:05:52 +0900

    windowの表示オプション修正

--- src/background.js
+++ src/background.js
@@ -1,8 +1,12 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
-        'outerBounds': {
-            'width': 400,
-            'height': 500,
-        }
+        outerBounds: {
+            width: 220,
+            height: 150,
+        },
+        frame: {
+            type: "none",
+        },
+        alwaysOnTop: true
     });
 });
